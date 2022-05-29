882d8b52a721912718f8defef974ca0a2caa0102
bbrunoo <brunoedsanntos@gmail.com>
2022-05-29 13:33:17 -0300

    atv06

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double pesoHomem = 0.0;
+double altura = 0.0;
+
+Console.WriteLine("Digite o seu peso (Homem)");
+pesoHomem = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o sua altura (Homem)");
+altura = Convert.ToDouble(Console.ReadLine());
+
+double pesoIdeal =  (72.7 * altura) - 58;
+
+Console.WriteLine($"Seu peso ideal é: {pesoIdeal}");
+
+
